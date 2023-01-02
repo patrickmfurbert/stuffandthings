28ab2884f412001e355999960a2de9fe8dc8a8a3
Patrick Furbert <patrickmfurbert@gmail.com>
2023-01-01 23:55:58 -0500

    feat: initial commit

---
+++ src/main/java/com/patrickfurbert/stuffandthings/StuffandthingsApplication.java
@@ -0,0 +1,13 @@
+package com.patrickfurbert.stuffandthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffandthingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuffandthingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrickfurbert/stuffandthings/StuffandthingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickfurbert.stuffandthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuffandthingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
